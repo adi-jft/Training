78d03640c5b2344b0ed87b7a01c0af375cf58493
Aditya Prakash <aditya.prakash@jellyfishtechnologies.com>
2022-11-28 15:15:29 +0530

    emp data synced with local storage

--- empAPIasyncawait/api.js
+++ empAPIasyncawait/api.js
@@ -1,10 +1,16 @@
 function employee(){
-    this.emparr=[];
+    // this.emparr=[];
+    this.emparr=JSON.parse(localStorage.getItem("obj"))||[];
 
     this.post=async (obj)=>{
         let p = new Promise((resolve, reject)=>{
             setTimeout(()=>{
                 this.emparr.push(obj);
+
+                let objSerialized=JSON.stringify(this.emparr);
+                localStorage.setItem("obj", objSerialized);
+                console.log(localStorage);
+                
                 resolve(this.emparr);
             }, 2000);
         });
@@ -29,6 +35,11 @@ function employee(){
                 this.emparr[ind].name=obj.name;
                 this.emparr[ind].job=obj.job;
                 this.emparr[ind].salary=obj.salary;
+
+                let objSerialized=JSON.stringify(this.emparr);
+                localStorage.setItem("obj", objSerialized);
+                console.log(localStorage);
+                
                 resolve(this.emparr);
             }, 2000);
             console.log(this.emparr);
@@ -39,9 +50,13 @@ function employee(){
     this.delete=async (eid)=>{
         let p=new Promise((resolve, reject)=>{
             setTimeout(()=>{
-                this.emparr.splice(
-                    this.emparr.findIndex((a)=>a.id==eid), 1);
-                    resolve(this.emparr);
+                this.emparr.splice(this.emparr.findIndex((a)=>a.id==eid), 1);
+
+                let objSerialized=JSON.stringify(this.emparr);
+                localStorage.setItem("obj", objSerialized);
+                console.log(localStorage);
+
+                resolve(this.emparr);
             }, 2000);
             console.log(this.emparr);
         });
